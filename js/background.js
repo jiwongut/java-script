1221872ecd7d14452a96a10c7a0c11818a69492d
jiwongut <wony4094@naver.com>
2022-03-10 19:15:32 +0900

    ~7.3 todo list

--- js/background.js
+++ js/background.js
@@ -1,7 +1,9 @@
 const images = [
-    "3.jpeg",
-    "4.jpeg",
-    "5.jpeg",
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
--- js/todo.js
+++ js/todo.js
@@ -2,20 +2,40 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
+const toDos = [];
+
+function saveToDos(){ //작성한 todo 를 저장함.
+    localStorage.setItem("todos", JSON.stringify(toDos)); //Stringify 문자화
+}
+
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
+    const button = document.createElement("button");
+    button.innerText = "x";
+    button.addEventListener("click",deleteToDo);
     li.appendChild(span); //li 안에 span 넣기.
+    li.appendChild(button);
     span.innerText = newTodo;
     toDoList.appendChild(li);
+
 }
+
 
 function handleToDoSubmit(event){
     event.preventDefault();
     //console.log(toDoInput.value);
     const newTodo = toDoInput.value; //새로운 todo 를 변수에 저장함.
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);

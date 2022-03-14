8ae603cb2f64064f739a16382aa7ec111953d797
jiwongut <wony4094@naver.com>
2022-03-14 20:02:22 +0900

    deleteTodo

--- js/todo.js
+++ js/todo.js
@@ -2,40 +2,75 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
-const toDos = [];
+let toDos = [];
+const TODOS_KEY = "todos";
+
+/*function sayHello(item){
+    console.log("this is the ", item);
+}*/
 
 function saveToDos(){ //작성한 todo 를 저장함.
-    localStorage.setItem("todos", JSON.stringify(toDos)); //Stringify 문자화
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); //Stringify 문자화
 }
 
 function deleteToDo(event){
     const li = event.target.parentElement;
+    toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
     li.remove();
+    saveToDos();
+    //console.log(typeof li.id); = string
 }
 
-
-function paintToDo(newTodo){
+function paintToDo(newTodo){ //newTodo는 객체
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
     const button = document.createElement("button");
     button.innerText = "x";
     button.addEventListener("click",deleteToDo);
     li.appendChild(span); //li 안에 span 넣기.
     li.appendChild(button);
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     toDoList.appendChild(li);
 
 }
-
 
 function handleToDoSubmit(event){
     event.preventDefault();
     //console.log(toDoInput.value);
     const newTodo = toDoInput.value; //새로운 todo 를 변수에 저장함.
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObject = {
+        text: newTodo,
+        id: Date.now(),
+    };
+    toDos.push(newTodoObject);
+    paintToDo(newTodoObject);
     saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+if(savedToDos !== null) {
+    const parsedToDOs = JSON.parse(savedToDos);
+    toDos = parsedToDOs; //update
+    parsedToDOs.forEach(paintToDo); //function을 더 간결하게 "arrow function"
+    //console.log(parsedToDOs);
+}
+
+/*지우고 싶은 item을 제외한다. filter
+ex1) 
+function sexyFilter(){
+    return true or false
+    return !== 3 3만 제외하고 반환
+}
+true > [1, 2, 3, 4, 5].filter(sexyFilter)
+false > [] 반환.
+
+ex2)
+const arr = ["a", "b", "c"]
+function sexyFilter(food){ return food !== "a"}
+arr.filter(sexyFilter)
+["b", "c"] 반환.
+*/
